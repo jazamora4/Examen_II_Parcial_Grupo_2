10f3c5a404acce302b441ed5476061085c27cab5
Jose Andres Zamora Miranda <43786193+jazamora4@users.noreply.github.com>
2021-08-04 19:57:56 -0500

    Project Init

---
+++ src/main/java/ec/edu/espe/distribuidas/examen/grupo2/ExamenGrupo2Application.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.examen.grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenGrupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenGrupo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/distribuidas/examen/grupo2/ExamenGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.examen.grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenGrupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
