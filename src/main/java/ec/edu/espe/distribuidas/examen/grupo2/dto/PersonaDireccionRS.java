eb65edbd877de1071d587bd23129b3af4418dfa1
Jose Andres Zamora Miranda <43786193+jazamora4@users.noreply.github.com>
2021-08-04 21:49:53 -0500

    Response Objects

--- src/main/java/ec/edu/espe/distribuidas/examen/grupo2/dto/PersonaDireccionRS.java
+++ src/main/java/ec/edu/espe/distribuidas/examen/grupo2/dto/PersonaDireccionRS.java
@@ -1,4 +1,19 @@
 package ec.edu.espe.distribuidas.examen.grupo2.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PersonaDireccionRS {
+
+    private String callePrincipal;
+    private String numeracion;
+    private String calleSecundaria;
+    private String barrio;
+    private String porOmision;
+    private String nombreTipoDireccion;
+    private String observacionTipoDireccion;
+    private String estadoTipoDireccion;
+
 }
--- src/main/java/ec/edu/espe/distribuidas/examen/grupo2/dto/PersonaRS.java
+++ src/main/java/ec/edu/espe/distribuidas/examen/grupo2/dto/PersonaRS.java
@@ -1,5 +1,13 @@
 package ec.edu.espe.distribuidas.examen.grupo2.dto;
 
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PersonaRS {
-    
+    private Integer codigo;
+    private String nombres;
+    private List<PersonaDireccionRS> direcciones;
 }
